a92a5782990a673a882dc32c762fb7d210920ebe
Nazar Vinnichuk <nazar@vinnich.uk>
2022-12-29 15:40:18 +0200

    Assert that error is not raised in test/deref.c.

--- test/deref.c
+++ test/deref.c
@@ -7,10 +7,15 @@
 
 #define PATH "/dev/null"
 
+void error_callback_function(void *parameter) { assert(parameter); }
+
 int main() {
+  struct callback *error_callback =
+      create_callback(error_callback_function, NULL);
+
   int fd = open(PATH, O_RDONLY);
-  assert(!strcmp(deref_fd(fd, NULL), PATH));
+  assert(!strcmp(deref_fd(fd, error_callback), PATH));
 
   int pid = getpid();
-  assert(deref_pid(pid, NULL));
+  assert(deref_pid(pid, error_callback));
 }
