4c12653034c94b3cbc080df8552a0602f856d9c4
Nazar Vinnichuk <nazar@vinnich.uk>
2023-01-08 14:31:56 +0200

    Remove some boilerplate from validation.lua.

--- lua/validation.lua
+++ lua/validation.lua
@@ -1,16 +1,17 @@
 assert(type(store) == 'string', 'store must be a string')
 assert(type(version_pattern) == 'string', 'version_pattern must be a string')
 
-function assert_is_size(value, name)
+function assert_is_size(name)
+  local value = _G[name]
   assert(
     math.tointeger(value) and math.tointeger(value) >= 0,
-    name .. 'must be a positive integer'
+    name .. ' must be a positive integer'
   )
 end
 
-assert_is_size(version_max_length, 'version_max_length')
-assert_is_size(path_length_guess, 'path_length_guess')
-assert_is_size(max_pid_guess, 'max_pid_guess')
+assert_is_size('version_max_length')
+assert_is_size('path_length_guess')
+assert_is_size('max_pid_guess')
 
 assert(type(editors) == 'table', 'editors must be a table')
 for editor, _ in pairs(editors) do
