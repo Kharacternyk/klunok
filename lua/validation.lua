6dcc6979186cd1605809c863a31c30cbd4f0a3b5
Nazar Vinnichuk <nazar@vinnich.uk>
2023-01-12 14:06:51 +0200

    Rename a Lua function.

--- lua/validation.lua
+++ lua/validation.lua
@@ -3,7 +3,7 @@ function assert_is_string(name)
   assert(type(value) == 'string', value .. ' must be a string')
 end
 
-function assert_is_size(name)
+function assert_is_positive(name)
   local value = _G[name]
   assert(
     math.tointeger(value) and math.tointeger(value) >= 0,
@@ -15,10 +15,10 @@ assert_is_string('store')
 assert_is_string('queue')
 assert_is_string('version_pattern')
 
-assert_is_size('debounce_seconds')
-assert_is_size('version_max_length')
-assert_is_size('path_length_guess')
-assert_is_size('max_pid_guess')
+assert_is_positive('debounce_seconds')
+assert_is_positive('version_max_length')
+assert_is_positive('path_length_guess')
+assert_is_positive('max_pid_guess')
 
 assert(type(editors) == 'table', 'editors must be a table')
 for editor, _ in pairs(editors) do
