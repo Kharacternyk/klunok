774b19ae9f7628bebd1f2d935412ade5eb62a41c
Nazar Vinnichuk <nazar@vinnich.uk>
2023-01-24 19:01:37 +0200

    Add section headers in config.lua.

--- lua/config.lua
+++ lua/config.lua
@@ -1,12 +1,10 @@
+-- Reloadable settings
 store_root = './klunok/store'
 queue_path = './klunok/queue'
 version_pattern = 'v%Y-%m-%d-%H-%M'
 debounce_seconds = 60
 version_max_length = 80
 path_length_guess = 1024
-max_pid_guess = 2^16
-elf_interpreter_count_guess = 1
-executable_count_guess = 128
 editors = {
   vi = true,
   vim = true,
@@ -15,4 +13,10 @@ editors = {
   ["soffice.bin"] = true,
 }
 
+-- Not reloadable settings
+max_pid_guess = 2^16
+elf_interpreter_count_guess = 1
+executable_count_guess = 128
+
+-- Compile-time settings
 circuit_breaker_seconds = 5
