0edc0eaaf390678b7aac6f23669dd847c2c6b2a1
Nazar Vinnichuk <nazar@vinnich.uk>
2023-02-15 21:02:11 +0200

    Remove an unfinished component.

--- src/identity.c
+++
@@ -1,7 +0,0 @@
-#include "identity.h"
-#include <pwd.h>
-
-struct identity *create_identity(const char *user_name, struct trace *trace) {
-  /*FIXME: MT-Unsafe*/
-  struct passwd *passwd = getpwnam(user_name);
-}
