08dd40a45ac2628eaca5aa3199a3859f2cb9e52b
Nazar Vinnichuk <nazar@vinnich.uk>
2023-02-22 18:40:12 +0200

    Optimize duplicates in set.

--- src/set.c
+++ src/set.c
@@ -1,11 +1,13 @@
 #include "set.h"
 #include "messages.h"
 #include "trace.h"
+#include <assert.h>
 #include <stdlib.h>
 #include <string.h>
 
 struct entry {
   char *value;
+  size_t count;
   struct entry *next;
 };
 
@@ -49,37 +51,40 @@ static struct entry **get_head(const char *value, const struct set *set) {
   return &(set->entries[hashed_value % set->size]);
 }
 
-bool is_in_set(const char *value, const struct set *set) {
-  struct entry *entry = *get_head(value, set);
+static struct entry **get_entry(const char *value, const struct set *set) {
+  struct entry **entry = get_head(value, set);
 
-  while (entry) {
-    if (strcmp(value, entry->value) == 0) {
-      return true;
+  while (*entry) {
+    assert((*entry)->count > 0);
+    if (strcmp(value, (*entry)->value) == 0) {
+      return entry;
     }
-    entry = entry->next;
+    entry = &((*entry)->next);
   }
 
-  return false;
+  return entry;
+}
+
+bool is_in_set(const char *value, const struct set *set) {
+  return *get_entry(value, set);
 }
 
 bool is_unique_within_set(const char *value, const struct set *set) {
-  size_t count = 0;
-  struct entry *entry = *get_head(value, set);
-
-  while (entry) {
-    if (strcmp(value, entry->value) == 0) {
-      ++count;
-      if (count == 2) {
-        return false;
-      }
-    }
-    entry = entry->next;
-  }
-
-  return true;
+  struct entry **entry = get_entry(value, set);
+  return !*entry || (*entry)->count == 1;
 }
 
 void add_to_set(const char *value, struct set *set, struct trace *trace) {
+  if (!ok(trace)) {
+    return;
+  }
+
+  struct entry **entry = get_entry(value, set);
+  if (*entry) {
+    ++(*entry)->count;
+    return;
+  }
+
   char *value_copy = TNULL(strdup(value), trace);
   struct entry *new_entry = TNULL(malloc(sizeof(struct entry)), trace);
   if (!ok(trace)) {
@@ -90,6 +95,7 @@ void add_to_set(const char *value, struct set *set, struct trace *trace) {
 
   new_entry->next = *head;
   new_entry->value = value_copy;
+  new_entry->count = 1;
 
   *head = new_entry;
 }
@@ -99,18 +105,19 @@ void remove_from_set(const char *value, struct set *set, struct trace *trace) {
     return;
   }
 
-  struct entry **entry = get_head(value, set);
-  while (*entry && strcmp((*entry)->value, value)) {
-    entry = &((*entry)->next);
-  }
+  struct entry **entry = get_entry(value, set);
   if (!*entry) {
     return throw_static(messages.set.not_in_set, trace);
   }
 
-  struct entry *new_next = (*entry)->next;
-  free((*entry)->value);
-  free(*entry);
-  *entry = new_next;
+  --(*entry)->count;
+
+  if (!(*entry)->count) {
+    struct entry *new_next = (*entry)->next;
+    free((*entry)->value);
+    free(*entry);
+    *entry = new_next;
+  }
 }
 
 void free_set(struct set *set) {
