7d4fbfb1aae8f0277d912e8522a3ce1e33a677c9
Nazar Vinnichuk <nazar@vinnich.uk>
2023-03-04 13:34:28 +0200

    Rewrite the split script in AWK.

--- lua/config.lua
+++ lua/config.lua
@@ -1,6 +1,8 @@
 --- Default configuration values
 
---_
+---
+--post--
+--
 function is_string(name)
   assert(type(_G[name]) == 'string', name .. ' must be a string')
 end
@@ -32,55 +34,142 @@ function declare(name, assertion, default)
     assertion(name)
   end
 end
---_
+
+---
 
 --- Helpers
 -- @section helpers
 
 --- Prefix used by default for all of the paths.
-prefix, is_string, nil, './klunok'
+---
+prefix
+--
+is_string
+--
+nil
+--
+'./klunok'
+---
 
 --- Prefix used by default for non-store paths.
-prefix_var, is_string, prefix .. '/var', nil
+---
+prefix_var
+--
+is_string
+--
+prefix .. '/var'
+--
+nil
+---
 
 --- Reloadable settings
 -- @section reloadable
 
 --- Root of the store.
-store_root, is_string, prefix .. '/store', nil
+---
+store_root
+--
+is_string
+--
+prefix .. '/store'
+--
+nil
+---
 
 --- Path to the queue (used for debouncing).
-queue_path, is_string, prefix_var .. '/queue', nil
+---
+queue_path
+--
+is_string
+--
+prefix_var .. '/queue'
+--
+nil
+---
 
 --- Path to the journal.
-journal_path, is_nil_or_string, prefix_var .. '/journal', nil
+---
+journal_path
+--
+is_nil_or_string
+--
+prefix_var .. '/journal'
+--
+nil
+---
 
 --- Pattern of timestamps in the journal.
 -- @see strftime(3)
 -- @static
-journal_timestamp_pattern, is_string, nil, '%Y-%m-%d-%H-%M'
+---
+journal_timestamp_pattern
+--
+is_string
+--
+nil
+--
+'%Y-%m-%d-%H-%M'
+---
 
 --- Pattern of file versions in the store.
 -- @see strftime(3)
 -- @static
-version_pattern, is_string, 'v' .. journal_timestamp_pattern, nil
+---
+version_pattern
+--
+is_string
+--
+'v' .. journal_timestamp_pattern
+--
+nil
+---
 
 --- Time a file must not be edited for its version to be copied to the store.
-debounce_seconds, is_positive, nil, 60
+---
+debounce_seconds
+--
+is_positive
+--
+nil
+--
+60
+---
 
 --- Guess of the queue size.
-queue_size_guess, is_positive, debounce_seconds * 2, nil
+---
+queue_size_guess
+--
+is_positive
+--
+debounce_seconds * 2
+--
+nil
+---
 
 --- Guess of the average path length in the system.
-path_length_guess, is_positive, nil, 1024
+---
+path_length_guess
+--
+is_positive
+--
+nil
+--
+1024
+---
 
 --- Filenames of executables that are considered editors.
---
 -- @usage editors.ed = true
 -- editors['emacs-28.3'] = true
 -- editors.code = nil -- do not treat "code" as an editor
 -- @static
-editors, is_set_of_strings, nil, {
+---
+editors
+--
+is_set_of_strings
+--
+nil
+--
+{
   atom = true,
   code = true,
   codium = true,
@@ -113,56 +202,168 @@ editors, is_set_of_strings, nil, {
   ['.pluma-wrapped'] = true,
   ['.xed-wrapped'] = true,
 }
+---
 
 --- Paths to "history"-files.
 -- "History"-files are assumed to be append-only and only changes will be stored
 -- as new store versions.
---
 -- @usage history_paths["/home/nazar/.bash_history"] = true
 -- @static
-history_paths, is_set_of_strings, nil, {}
+---
+history_paths
+--
+is_set_of_strings
+--
+nil
+--
+{}
+---
 
---- Paths excluded from copying to the store
-excluded_paths, is_set_of_strings, nil, {}
+--- Paths excluded from copying to the store.
+---
+excluded_paths
+--
+is_set_of_strings
+--
+nil
+--
+{}
+---
 
---- Paths included in copying to the store
-included_paths, is_set_of_strings, nil, {}
+--- Paths included in copying to the store.
+---
+included_paths
+--
+is_set_of_strings
+--
+nil
+--
+{}
+---
 
 --- Prefix of the journal event. If `nil`, the event is not logged to the journal.
-event_open_exec_not_editor, is_nil_or_string, nil, nil
+---
+event_open_exec_not_editor
+--
+is_nil_or_string
+--
+nil
+--
+nil
+---
 
 --- Prefix of the journal event. If `nil`, the event is not logged to the journal.
-event_open_exec_editor, is_nil_or_string, nil, nil
+---
+event_open_exec_editor
+--
+is_nil_or_string
+--
+nil
+--
+nil
+---
 
 --- Prefix of the journal event. If `nil`, the event is not logged to the journal.
-event_open_exec_interpreter, is_nil_or_string, nil, nil
+---
+event_open_exec_interpreter
+--
+is_nil_or_string
+--
+nil
+--
+nil
+---
 
 --- Prefix of the journal event. If `nil`, the event is not logged to the journal.
-event_close_write_not_by_editor, is_nil_or_string, nil, nil
+---
+event_close_write_not_by_editor
+--
+is_nil_or_string
+--
+nil
+--
+nil
+---
 
 --- Prefix of the journal event. If `nil`, the event is not logged to the journal.
-event_close_write_by_editor, is_nil_or_string, nil, nil
+---
+event_close_write_by_editor
+--
+is_nil_or_string
+--
+nil
+--
+nil
+---
 
 --- Prefix of the journal event. If `nil`, the event is not logged to the journal.
-event_queue_head_deleted, is_nil_or_string, nil, nil
+---
+event_queue_head_deleted
+--
+is_nil_or_string
+--
+nil
+--
+nil
+---
 
 --- Prefix of the journal event. If `nil`, the event is not logged to the journal.
-event_queue_head_forbidden, is_nil_or_string, nil, nil
+---
+event_queue_head_forbidden
+--
+is_nil_or_string
+--
+nil
+--
+nil
+---
 
 --- Prefix of the journal event. If `nil`, the event is not logged to the journal.
-event_queue_head_stored, is_nil_or_string, nil, ''
+---
+event_queue_head_stored
+--
+is_nil_or_string
+--
+nil
+--
+''
+---
 
 --- Not reloadable settings.
 -- @section not-reloadable
 
 --- Guess of the maximum PID in the system while klunok is running.
-max_pid_guess, is_positive, nil, 2^15
+---
+max_pid_guess
+--
+is_positive
+--
+nil
+--
+2^15
+---
 
 --- Guess of how many ELF-iterpreters are there in the system.
-elf_interpreter_count_guess, is_positive, nil, 1
+---
+elf_interpreter_count_guess
+--
+is_positive
+--
+nil
+--
+1
+---
 
 --- Guess of how many executable files are run in the system while klunok is running.
-executable_count_guess, is_positive, nil, 128
+---
+executable_count_guess
+--
+is_positive
+--
+nil
+--
+128
+---
 
 -- Compile-time settings
 circuit_breaker_seconds = 5
---
+++ lua/split.awk
@@ -0,0 +1,27 @@
+BEGIN {
+    RS = "\n---\n"
+    FS = "\n--\n"
+}
+{
+    if (NF > 1) {
+        if ($1 == "--post--") {
+            printf "%s", $2 > post
+        } else {
+            gsub("\n", " ", $2)
+            gsub("\n", " ", $3)
+            gsub("\n", " ", $4)
+
+            printf "%s = %s\n", $1, $4 > pre
+            printf "declare('%s', %s, %s)\n", $1, $2, $3 > post
+            printf "\n--\n-- * Type: `%s`\n", $2 > doc
+            if ($3 != "nil") {
+                printf "-- * Reactive default: `%s`\n", $3 > doc
+            } else {
+                printf "-- * Default: `%s`\n", $4 > doc
+            }
+            printf "%s = nil\n", $1 > doc
+        }
+    } else {
+        printf "%s", $0 > doc
+    }
+}
