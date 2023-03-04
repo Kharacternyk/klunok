09b9440eed084ca66e344f4f512f70def438eddd
Nazar Vinnichuk <nazar@vinnich.uk>
2023-03-04 13:58:55 +0200

    Change the style of the docs.

--- lua/ldoc.lua
+++ lua/ldoc.lua
@@ -3,6 +3,7 @@ title = 'klunok configuration'
 format = 'markdown'
 no_lua_ref = true
 not_luadoc = true
+style = '!pale'
 
 local man_pattern = 'https://man.archlinux.org/man/core/man-pages/%s.%s.en'
 custom_see_handler('^([%w_]+)%((%d)%)$', function(name, section)
