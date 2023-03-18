916ba53f7307f5f1ca80cd027e9c0dbfa5b64a33
Nazar Vinnichuk <nazar@vinnich.uk>
2023-03-18 15:25:42 +0200

    Store project version in a separate file.

--- default.nix
+++ default.nix
@@ -7,7 +7,7 @@
 , doCheck ? false
 }: stdenv.mkDerivation {
   pname = "klunok";
-  version = "0.1.0-" + (if lua == null then "no-lua" else "lua-${lua.version}");
+  version = builtins.readFile ./version + (if lua == null then "no-lua" else "lua-${lua.version}");
   src = ./.;
 
   inherit doCheck;
