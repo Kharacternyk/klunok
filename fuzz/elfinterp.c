e951cc0eed1e7626cc9f98c07ee0fcc5faf5225c
Nazar Vinnichuk <nazar@vinnich.uk>
2023-04-04 21:28:57 +0300

    Remove some redundant includes.

--- fuzz/elfinterp.c
+++ fuzz/elfinterp.c
@@ -1,9 +1,6 @@
 #include "elfinterp.h"
 #include "trace.h"
 #include <assert.h>
-#include <fcntl.h>
-#include <stdlib.h>
-#include <unistd.h>
 
 void fuzz_elfinterp() {
   struct trace *trace = create_trace();
