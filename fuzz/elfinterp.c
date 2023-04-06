1e83d91c49f51d5d16f763dbf869a720a6fdafae
Nazar Vinnichuk <nazar@vinnich.uk>
2023-04-06 20:46:11 +0300

    Switch to persistent fuzzing.

--- fuzz/elfinterp.c
+++ fuzz/elfinterp.c
@@ -1,9 +1,28 @@
+#define _GNU_SOURCE
 #include "elfinterp.h"
 #include "trace.h"
 #include <assert.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <unistd.h>
 
-void fuzz_elfinterp() {
+int LLVMFuzzerTestOneInput(const char *input, size_t size) {
   struct trace *trace = create_trace();
   assert(trace);
-  get_elf_interpreter(0, trace);
+
+  int fd = memfd_create("", O_RDWR);
+  size_t total_written = 0;
+  while (total_written < size) {
+    size_t iter_written = write(fd, input + total_written, size);
+    if (iter_written < 0) {
+      assert(0);
+    }
+    total_written += iter_written;
+  }
+
+  lseek(fd, 0, SEEK_SET);
+
+  get_elf_interpreter(fd, trace);
+
+  return 0;
 }
