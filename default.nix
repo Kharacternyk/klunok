78f4f238a1d35638e00e2ace09500a130a82f833
Nazar Vinnichuk <nazar@vinnich.uk>
2023-06-09 22:45:23 +0300

    Add FTS to musl builds.

--- default.nix
+++ default.nix
@@ -1,13 +1,16 @@
-{ stdenv
+{ lib
+, stdenv
 , meson
 , ninja
 , pkg-config
 , lua
 , valgrind-light
+, musl-fts
 , doCheck ? false
 }: stdenv.mkDerivation {
   pname = "klunok";
-  version = builtins.readFile ./version + (if lua == null then "no-lua" else "lua-${lua.version}");
+  version = builtins.readFile ./version
+    + (if lua == null then "no-lua" else "lua-${lua.version}");
   src = ./.;
 
   inherit doCheck;
@@ -16,6 +19,8 @@
     meson
     ninja
     pkg-config
+  ] ++ lib.optionals (stdenv.targetPlatform.libc == "musl") [
+    musl-fts
   ];
   buildInputs = [
     lua
