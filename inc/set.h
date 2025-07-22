c3cf11beeac016d8beffe5c853fa51f870aab8a4
Nazar Vinnichuk <nazar@vinnich.uk>
2025-07-22 22:52:16 +0300

    Reapply "Allow attaching metadata to set items."
    This reverts commit d9318aee4ad6d52f177e905c02a200a3ca6caad6.

--- inc/set.h
+++ inc/set.h
@@ -11,12 +11,17 @@ bool is_empty(const struct set *set)
     __attribute__((pure, nonnull, warn_unused_result));
 
 void add(const char *value, struct set *set, struct trace *trace);
+void add_with_metadata(const char *value, unsigned metadata, struct set *set,
+                       struct trace *trace);
 
 bool is_within(const struct buffer_view *value, const struct set *set)
     __attribute__((pure, nonnull, warn_unused_result));
 void pop(const struct buffer_view *value, struct set *set)
     __attribute__((nonnull));
 size_t get_count(const struct buffer_view *value, const struct set *set)
+    __attribute__((pure, nonnull, warn_unused_result));
+unsigned get_last_metadata(const struct buffer_view *value,
+                           const struct set *set)
     __attribute__((pure, nonnull, warn_unused_result));
 
 void free_set(struct set *set);
