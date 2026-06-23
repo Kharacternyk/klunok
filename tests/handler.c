c2560c01d136aa8d5bfc2da4fc4cd3181997cfc3
Nazar Vinnichuk <nazar@vinnich.uk>
2026-06-24 00:13:50 +0300

    Simplify a test.

--- tests/handler.c
+++ tests/handler.c
@@ -3,7 +3,6 @@
 #include "trace.h"
 #include <assert.h>
 #include <fcntl.h>
-#include <string.h>
 #include <sys/stat.h>
 #include <unistd.h>
 
@@ -13,15 +12,6 @@
 #define F2 TEST_ROOT "/lua/empty.lua"
 #define EMPTY "empty"
 #define IN_STORE(PATH) "./klunok/store/" PATH "/version"
-#define WORKING_COPY(PATH) "./klunok/store/" PATH "/working-copy"
-
-static void assert_symlink_target(const char *path, const char *target) {
-  char actual[4096];
-  ssize_t length = readlink(path, actual, sizeof(actual) - 1);
-  assert(length >= 0);
-  actual[length] = 0;
-  assert(!strcmp(actual, target));
-}
 
 void test_handler(struct trace *trace) {
   struct handler *handler = load_handler(CONFIG, 1, trace);
@@ -51,10 +41,6 @@ void test_handler(struct trace *trace) {
 
   assert(access(IN_STORE(F1) ".lua", F_OK) == 0);
   assert(access(IN_STORE(F2) ".lua", F_OK) == 0);
-  assert_symlink_target(WORKING_COPY(F1) ".lua", F1);
-  assert_symlink_target(WORKING_COPY(F2) ".lua", F2);
-  struct stat working_copy_stat;
-  assert(lstat(WORKING_COPY(F2) ".lua", &working_copy_stat) == 0);
 
   handle_close_write(getpid(), fd, handler, trace);
   assert(ok(trace));
@@ -64,9 +50,6 @@ void test_handler(struct trace *trace) {
   assert(pause < 0);
 
   assert(access(IN_STORE(F2) "-1.lua", F_OK) == 0);
-  struct stat new_working_copy_stat;
-  assert(lstat(WORKING_COPY(F2) ".lua", &new_working_copy_stat) == 0);
-  assert(working_copy_stat.st_ino == new_working_copy_stat.st_ino);
 
   close(fd);
   fd = open(EMPTY, O_CREAT, S_IRWXU);
@@ -80,7 +63,6 @@ void test_handler(struct trace *trace) {
   assert(ok(trace));
   assert(pause < 0);
   assert(access(IN_STORE(EMPTY), F_OK) != 0);
-  assert(access(WORKING_COPY(EMPTY), F_OK) != 0);
 
   close(fd);
   fd = open(EMPTY, O_CREAT, S_IWOTH);
@@ -92,7 +74,6 @@ void test_handler(struct trace *trace) {
   assert(ok(trace));
   assert(pause < 0);
   assert(access(IN_STORE(EMPTY), F_OK) != 0);
-  assert(access(WORKING_COPY(EMPTY), F_OK) != 0);
 
   close(fd);
   free_handler(handler);
@@ -109,7 +90,6 @@ void test_handler(struct trace *trace) {
   assert(pause < 0);
   assert(ok(trace));
   assert(access(IN_STORE(CONFIG_BASE) ".lua", F_OK) == 0);
-  assert_symlink_target(WORKING_COPY(CONFIG_BASE) ".lua", CONFIG);
 
   close(fd);
   free_handler(handler);
